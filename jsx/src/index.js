7bf2ff6f72acd82e1d87b72a3580f25414786150
Ali Raza Qureshi <ali.qureshi@techlogix.com>
2022-04-11 15:29:06 +0500

    Initial commit

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: "Click me" };
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
